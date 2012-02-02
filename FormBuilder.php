f1d1cd46f1263a4a045536eec1c40f594c70da3f
Tatyana Vakulenko <tvakulenko@gmail.com>
2012-02-02 17:11:09 +0200

    update

--- FormBuilder.php
+++ FormBuilder.php
@@ -18,7 +18,7 @@ class FormBuilder
 {
     public $avalibleInputAttrs = array(
         'id', 'class', 'name', 'type', 'value', 'placeholder', 'required', 'size',
-        'src', 'align', 'disabled', 'readonly'
+        'src', 'align', 'disabled', 'readonly', 'checked'
     );
     public $avalibleTextareaAttrs = array(
         'id', 'class', 'name', 'cols', 'rows', 'required', 'disabled', 'readonly'
@@ -38,70 +38,81 @@ class FormBuilder
     }
 
     public function buildForm($obj) 
-    {
+    {       
         require_once 'header.php';
-        echo "<form name='" . $obj->name . "' method='" . $obj->method . "' action='" . $obj->action . "' >";
-        echo $this->buildItems($obj->item);
-        echo "</form>";
+        foreach($obj->form as $form){
+            echo "<form name='" . $form->name . "' method='" . $form->method . "'
+            action='" . $form->action . "' enctype='".$form->enctype."' >".$this->buildItems($form->name, $form->item)."</form>";
+        }
         require_once 'footer.php';
     }
-    public function buildItems($items)
+    public function buildItems($formName, $items)
     {
         $str = '';
         foreach ($items as $key => $item)
         {
             if (isset($item->label))
+            {
+                $str .= '<label for="'.$item->id.'">' . $item->label.'</label>';
+                if (substr_count($item->class, 'required') > 0)
                 {
-                    $str .= '<label for="'.$item->id.'">' . $item->label.'</label>';
-                    if (substr_count($item->class, 'required') > 0)
-                    {
-                        $str .= '<em>*</em>';
-                    }
-                    else{
-                        $str .= '<em>&nbsp;</em>';
-                    }
-                }
-                if(isset($item->elem)){
-                    $render = 'render' . ucfirst($item->elem);
-                    $str .= $this->$render($item);
+                    $str .= '<em>*</em>';
                 }
                 else
                 {
-                    foreach($item->item as $val)
-                    {
-                        $render = 'render' . ucfirst($val->elem);
-                        $str .= $this->$render($val);
-                    }
+                    $str .= '<em>&nbsp;</em>';
                 }
-            
+            }
+            if(isset($item->elem))
+            {
+                $render = 'render' . ucfirst($item->elem);
+                $str .= $this->$render($formName, $item);
+            }
+            else
+            {
+                foreach($item->item as $val)
+                {
+                    $render = 'render' . ucfirst($val->elem);
+                    $str .= $this->$render($formName, $val);
+                }
+                    
+            }
+            if (substr_count($item->class, 'required') > 0)
+            {
+                    $str .= '<span class="error_'.$formName.'_'.$item->name.'"></span>';
+            }
             $str .=  '<br>';
         }
         return $str;
     }
 
-    protected function renderInput($attrs) 
+    protected function renderInput($formName, $attrs)
     {
-        return "<input ". $this->checkExistAttr($attrs)." >";
+        return "<input ". $this->checkExistAttr($formName, $attrs)." >";
 
     }
 
-    protected function renderTextarea($attrs) 
+    protected function renderTextarea($formName, $attrs)
     {
-        return "<textarea".$this->checkExistAttr($attrs)." >" . $attrs->value . "</textarea>";
+        return "<textarea".$this->checkExistAttr($formName, $attrs)." >" . $attrs->value . "</textarea>";
     }
 
-    protected function renderSelect($attrs) 
+    protected function renderSelect($formName, $attrs)
     {
-        $str = "<select ". $this->checkExistAttr($attrs)." >";
+        $str = "<select ". $this->checkExistAttr($formName, $attrs)." >";
         foreach ($attrs->option as $opt) 
         {
-            $str .= "<option value='" . $opt->value . "'>" . $opt->text . "</option>";
+            $selected = '';
+            if($opt->selected){
+                $selected .= 'selected';
+            }
+            $str .= "<option value='" . $opt->value . "' ".$selected.">" . $opt->text . "</option>";
         }
         $str .= "</select>";
         return $str;
     }
 
-    protected function checkExistAttr($attrs) 
+    protected function checkExistAttr($formName, $attrs)
     {
         $avalibleAttrs = 'avalible' . ucfirst($attrs->elem) . 'Attrs';
         $str = '';
@@ -109,6 +120,10 @@ class FormBuilder
         {
             if (in_array($key, $this->$avalibleAttrs)) 
             {
+                if($key == 'name')
+                {
+                   $val = $formName."_".$val;
+                }
                 $str .= ' ' . $key . "='" . $val . "' ";
             }
         }
