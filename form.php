5d8f577a9cc4c43d3b3fee1db02ca9742bae4d7a
Tatyana Vakulenko <tvakulenko@gmail.com>
2012-02-02 17:13:06 +0200

    update

--- form.php
+++ form.php
@@ -5,10 +5,20 @@ require_once 'FormBuilder.php';
 $xml = 'form.xml';
 $form = new FormBuilder();
 $obj = $form->getObjFromXml($xml);
+        
 if (is_object($obj)) {
     $data = $form->buildForm($obj);
     
-    
+    if (isset($_REQUEST)) {
+        print "<pre>";
+        var_dump($_REQUEST);
+        print "</pre>";
+       
+    }
+    //print_r($data);
+     print "<pre>";
+        var_dump($_FILES);
+        print "</pre>";
 } else {
     throw new Exception('You must recieve xml object');
 }
